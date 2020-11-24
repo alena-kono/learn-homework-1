4c12b0922d5bbb341d7caa463ce418c7719b90a6
Alena Konovalova <konovalova.alena@gmail.com>
2020-11-24 19:37:53 +0300

    solve 4_while1.py

--- 4_while1.py
+++ 4_while1.py
@@ -4,18 +4,20 @@
 
 Цикл while: hello_user
 
-* Напишите функцию hello_user(), которая с помощью функции input() спрашивает 
+* Напишите функцию hello_user(), которая с помощью функции input() спрашивает
   пользователя “Как дела?”, пока он не ответит “Хорошо”
-   
+
 """
 
 
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
 
-    
+  user_answer = ''
+
+  while user_answer != 'Fine':
+      user_answer = input('How are you?\n')
+
+
+
 if __name__ == "__main__":
     hello_user()
